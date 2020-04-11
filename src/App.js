1e077fc067e7dc0216157aeb853eb7bc9ffe4250
esteban1990 <55974184+esteban1990@users.noreply.github.com>
2020-04-11 14:00:51 -0400

    Todo-List-sin-style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoInput from "./componentes/todoInput";
+import TodoItem from './todoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+      this.state = {
+        todos : [
+          {id:0, text:"Make your bed"},
+          {id:0, text:"Make your bed"},
+          {id:0, text:"Make your bed"}
+        ],
+        nextId : 3
+      }
+
+      this.addTodo = this.addTodo.bind(this);
+      this.removeTodo = this.removeTodo.bind(this);
+  }
+
+      addTodo(todoText){
+        let todos = this.state.todos.slice();
+        todos.push({id: this.state.nextId, text: todoText})
+        this.setState({
+          todos : todos,
+          nextId : ++this.state.nextId
+        })
+      }
+
+      removeTodo(id){
+        this.setState({
+          todos: this.state.todos.filter((todo, index)=>todo.id !==id)
+        })
+      }
+
+
+render(){
+  return(
+      <div className="Wrapper">
+        <div className="todo-wrapper">
+          <div className="card">
+            <div className="card-header">
+              <h1>todo list</h1>
+              <TodoInput todoText="" addTodo={this.addTodo}/>
+              <ul>
+                {
+                  this.state.todos.map((todo)=>{
+                    return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo} />
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+  )
+}
+
+
 }
 
 export default App;
+
+
+
+
+
+
+
---
+++ src/todoItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+
+class TodoItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    removeTodo(id) {
+        this.props.removeTodo(id)
+
+    }
+
+    render() {
+        return (
+
+            <div className="todoWrapper">
+                <button className="removeTodo" onClick={(e) => this.removeTodo(this.props.id)}>Eliminar Todo</button>{this.props.todo.text}
+            </div>
+        )
+
+    }
+
+}
+
+export default TodoItem;
